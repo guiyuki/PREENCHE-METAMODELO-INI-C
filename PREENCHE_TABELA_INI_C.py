e83b1d5d639301aa645b6e3a57b9f53f889d078b
guiyuki <36683059+guiyuki@users.noreply.github.com>
2021-10-28 09:12:51 -0300

    Add files via upload

--- PREENCHE_TABELA_INI_C.py
+++ PREENCHE_TABELA_INI_C.py
@@ -8,45 +8,54 @@ Criar um arquivo .csv somente com os valores.
 Deixar o script e o arquivo na mesma pasta.
 Possiveis erros.
 '''
-arquivo =  open(input('Nome do arquivo .csv\n O arquivo deve estar na mesma pasta que esse arquivo! \n USAR UMA VEZ PARA CADA PAVIMENTO!!\nDEVE CONTER O NOME EXATO DO ARQUIVO SENSÍVEL ÀS minúsculas!!!\n')+ ".csv")
-
-data = csv.reader(arquivo,delimiter=';')
-
-linha =[]
-
-pyautogui.hotkey('alt','tab')
 
 def escrevevalor(lista=list):
     for i in range(len(lista)):
         VALOR=lista[i]
+        if (i==7 or i==8) and (VALOR=='S' or VALOR=='s'):
+            VALOR="ss"
+            pass
+        elif VALOR=='Perimetral':
+            VALOR='P'
+            pass
         if VALOR=='p':
             pyautogui.hotkey('tab')
-            time.sleep(0.05)
+            time.sleep(0.02)
             break
         elif VALOR=='':
             break
         elif i==1 and VALOR!='':
             pyautogui.hotkey('tab')
-            time.sleep(0.05)
-            pyautogui.write(VALOR, interval=0.02)
-            time.sleep(0.05)
+            time.sleep(0.02)
+            pyautogui.write(VALOR, interval=0.005)
+            time.sleep(0.02)
             pyautogui.hotkey('tab')
             pass
         elif i==(len(lista)-1):
-            pyautogui.write(VALOR, interval=0.02)
-            time.sleep(0.05)
+            pyautogui.write(VALOR, interval=0.005)
+            time.sleep(0.02)
             pyautogui.hotkey('tab')
         else:
-            pyautogui.write(VALOR, interval=0.02)
-            time.sleep(0.05)
+            pyautogui.write(VALOR, interval=0.005)
+            time.sleep(0.02)
             pyautogui.hotkey('tab')
             pass
         pass
     pass
 
-for row in data:
-    escrevevalor(row)
-    pass
+#codigo
 
-pyautogui.hotkey('alt','tab')
+def executar():
+    arquivo =  open(input('Nome do arquivo .csv\n O arquivo deve estar na mesma pasta que esse arquivo! \n USAR UMA VEZ PARA CADA PAVIMENTO!!\nDEVE CONTER O NOME EXATO DO ARQUIVO SENSÍVEL ÀS minúsculas!!!\n')+ ".csv")
+    data = csv.reader(arquivo,delimiter=';')
+    linha =[]
+    pyautogui.hotkey('alt','tab')
+    for row in data:
+        escrevevalor(row)
+        pass
+    pyautogui.hotkey('alt','tab')
+
+executar()
+executar()
+executar()
 
