561d4a2dfed6a17beb36e4965af55f10395a5116
guiyuki <36683059+guiyuki@users.noreply.github.com>
2021-10-28 15:54:31 -0300

    Add files via upload
    Novas funções e menos código.

--- PREENCHE_TABELA_INI_C.py
+++ PREENCHE_TABELA_INI_C.py
@@ -9,51 +9,72 @@ Deixar o script e o arquivo na mesma pasta.
 Possiveis erros.
 '''
 
-def escrevevalor(lista=list):
-    for i in range(len(lista)):
-        VALOR=lista[i]
-        if (i==7 or i==8) and (VALOR=='S' or VALOR=='s'):
-            VALOR="ss"
-            pass
-        elif VALOR=='Perimetral':
-            VALOR='P'
-            pass
-        if VALOR=='p':
-            pyautogui.hotkey('tab')
-            time.sleep(0.02)
-            break
-        elif VALOR=='':
-            break
-        elif i==1 and VALOR!='':
-            pyautogui.hotkey('tab')
-            time.sleep(0.02)
-            pyautogui.write(VALOR, interval=0.005)
-            time.sleep(0.02)
-            pyautogui.hotkey('tab')
-            pass
-        elif i==(len(lista)-1):
-            pyautogui.write(VALOR, interval=0.005)
-            time.sleep(0.02)
-            pyautogui.hotkey('tab')
-        else:
-            pyautogui.write(VALOR, interval=0.005)
-            time.sleep(0.02)
-            pyautogui.hotkey('tab')
-            pass
-        pass
-    pass
-
-#codigo
-
 def executar():
     arquivo =  open(input('Nome do arquivo .csv\n O arquivo deve estar na mesma pasta que esse arquivo! \n USAR UMA VEZ PARA CADA PAVIMENTO!!\nDEVE CONTER O NOME EXATO DO ARQUIVO SENSÍVEL ÀS minúsculas!!!\n')+ ".csv")
     data = csv.reader(arquivo,delimiter=';')
-    linha =[]
+    linha = data.line_num
     pyautogui.hotkey('alt','tab')
     for row in data:
-        escrevevalor(row)
+        escrevevalor(row,linha)
         pass
     pyautogui.hotkey('alt','tab')
+    pass
+pass
+
+def escrevevalor(lista=list,linha=int):
+    for i in range(len(lista)):
+        VALOR=lista[i]
+        #regras para linhas
+        #linha Fator Solar Vidro
+        if linha==9 and VALOR=='':
+            VALOR=str(0.5)
+            pass
+        #linha Uvidro
+        if linha==10 and VALOR=='':
+            VALOR=str(4)
+            pass
+        #linha tipo de zona - interna/perimetral
+        elif linha==5 and VALOR=='p':
+            VALOR='P'
+            pass
+        elif (i==7 or i==8) and (VALOR=='S' or VALOR=='s'):
+            VALOR="ss"
+            pass
+        elif VALOR=='Perimetral' or VALOR=='perimetral':
+            VALOR='P'
+            pass
+        #escrever
+        if i==1 and VALOR!='':
+            pyautogui.hotkey('tab')
+            escrever(VALOR)
+        else:
+            escrever(VALOR)
+        pass
+    pass
+pass
+
+def escrever(var=str):
+    V=.03
+    if var=='':
+        pass
+    elif var=='p':
+        time.sleep(V)
+        pyautogui.hotkey('tab')
+        pass
+    else:
+        pyautogui.write(var, interval=0.005)
+        time.sleep(V)
+        pyautogui.hotkey('tab')
+        print(var)
+        pass
+    pass
+pass
+
+    
+
+#codigo
+
+
 
 executar()
 executar()
